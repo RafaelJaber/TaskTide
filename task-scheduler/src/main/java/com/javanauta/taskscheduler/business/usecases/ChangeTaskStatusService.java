ba00d7d7908023c887f13b0830cb061a0e2f8a7f
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-29 10:24:13 -0300

    ✨ Adiciona suporte para atualização de tarefas e alteração de status de tarefas 📝 Adiciona novos DTOs e mappers para suportar as novas funcionalidades 🔧 Adiciona novos casos de uso para atualização e alteração de status de tarefas

--- task-scheduler/src/main/java/com/javanauta/taskscheduler/business/usecases/ChangeTaskStatusService.java
+++ task-scheduler/src/main/java/com/javanauta/taskscheduler/business/usecases/ChangeTaskStatusService.java
@@ -1,29 +1,29 @@
 package com.javanauta.taskscheduler.business.usecases;
 
+import com.javanauta.taskscheduler.business.dto.request.TaskStatusRequestDTO;
 import com.javanauta.taskscheduler.business.dto.response.TaskResponseDTO;
 import com.javanauta.taskscheduler.business.mapper.TaskMapper;
-import com.javanauta.taskscheduler.core.utils.JwtUtil;
 import com.javanauta.taskscheduler.infrastructure.entities.TaskEntity;
+import com.javanauta.taskscheduler.infrastructure.exceptions.ResourceNotFoundException;
 import com.javanauta.taskscheduler.infrastructure.repositories.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
-public class FindTasksByCurrentUserService {
+public class ChangeTaskStatusService {
 
     private final TaskRepository taskRepository;
     private final TaskMapper taskMapper;
-    private final JwtUtil jwtUtil;
 
-    @Transactional(readOnly = true)
-    public List<TaskResponseDTO> execute() {
-        String userEmail = jwtUtil.getCurrentUserEmail();
-        List<TaskEntity> tasks = this.taskRepository.findByUserEmail(userEmail);
-
-        return tasks.stream().map(taskMapper::toTaskResponseDTO).toList();
+    @Transactional
+    public TaskResponseDTO execute(TaskStatusRequestDTO dto, String id) {
+        TaskEntity task = taskRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Task with id " + id + " not found")
+        );
+        task.setStatus(dto.getStatus());
+        taskRepository.save(task);
+        return taskMapper.toTaskResponseDTO(task);
     }
 }
