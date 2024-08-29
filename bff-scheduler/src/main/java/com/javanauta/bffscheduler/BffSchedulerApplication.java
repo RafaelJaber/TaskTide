03df28e590a51f5a3bf547edc24a67349bac2459
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-29 17:17:42 -0300

    🔧 (gradle.yml): renomeia tarefas e diretórios para BFF Scheduling 🔧 (gradle.yml): atualiza dependências e configurações do BFF Scheduling 🔧 (gradle.yml): adiciona e configura arquivos de build.gradle e gradle-wrapper para Naming Server
    🔧 (naming-server/gradlew): Adiciona script de inicialização Gradle para sistemas POSIX.
    
    ✨ Adiciona suporte para o servidor Eureka no projeto de notificações e no agendador de tarefas
    🔧 Atualiza o nome da aplicação no arquivo de configuração do Eureka para refletir o microserviço correspondente
    🔧 Adiciona dependência do cliente Eureka no projeto de notificações e no agendador de tarefas
    🔧 Atualiza o nome da aplicação no arquivo de configuração do Eureka para refletir o microserviço correspondente
    
    ⬆️ (build.gradle): atualiza a versão do Spring Cloud para 2023.0.3
    ⬆️ (build.gradle): adiciona dependências do Spring Cloud Eureka Client e Spring Boot Actuator
    ⬆️ (build.gradle): adiciona gerenciamento de dependências do Spring Cloud
    ⬆️ (UserApplication.java): habilita o cliente de descoberta do Spring Cloud
    ⬆️ (application.yml): renomeia o aplicativo para user-microservice e configura o Eureka Server

--- bff-scheduler/src/main/java/com/javanauta/bffscheduler/BffSchedulerApplication.java
+++ bff-scheduler/src/main/java/com/javanauta/bffscheduler/BffSchedulerApplication.java
@@ -2,7 +2,9 @@ package com.javanauta.bffscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BffSchedulerApplication {
 
