fdc406696ea67cc91bd91c1c3d74b0add4176d98
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-29 09:24:19 -0300

    ✨ Adiciona casos de uso para encontrar tarefas por data de agendamento e usuário atual 📝 Adiciona classe de serviço FindTasksByCurrentUserService para buscar tarefas do usuário atual 📝 Adiciona classe de serviço FindTasksBySchedulingDateService para buscar tarefas por data de agendamento 📝 Adiciona método findBySchedulingDateBetween em TaskRepository 📝 Adiciona método findByUserEmail em TaskRepository

---
+++ task-scheduler/src/main/java/com/javanauta/taskscheduler/business/usecases/FindTasksByCurrentUserService.java
@@ -0,0 +1,28 @@
+package com.javanauta.taskscheduler.business.usecases;
+
+import com.javanauta.taskscheduler.business.dto.response.TaskResponseDTO;
+import com.javanauta.taskscheduler.business.mapper.TaskMapper;
+import com.javanauta.taskscheduler.core.utils.JwtUtil;
+import com.javanauta.taskscheduler.infrastructure.entities.TaskEntity;
+import com.javanauta.taskscheduler.infrastructure.repositories.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FindTasksByCurrentUserService {
+
+    private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
+    private final JwtUtil jwtUtil;
+
+
+    public List<TaskResponseDTO> execute() {
+        String userEmail = jwtUtil.getCurrentUserEmail();
+        List<TaskEntity> tasks = this.taskRepository.findByUserEmail(userEmail);
+
+        return tasks.stream().map(taskMapper::toTaskResponseDTO).toList();
+    }
+}
