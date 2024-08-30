c74a21201e9e2a70c32f89704fb6efe206ebf234
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-29 22:01:46 -0300

    ✨ Adiciona configuração do OpenAPI para documentação da API e segurança com JWT. Adiciona anotações de segurança nas interfaces dos controladores de tarefas e endereços de usuário.
    📝 (UserContactControllerOpenAPI.java): Adiciona anotações @Parameter(hidden = true) nos parâmetros de token para ocultar informações sensíveis
    📝 (UserControllerOpenAPI.java): Adiciona anotações @Parameter(hidden = true) nos parâmetros de token para ocultar informações sensíveis

--- bff-scheduler/src/main/java/com/javanauta/bffscheduler/infrastructure/openapi/UserContactControllerOpenAPI.java
+++ bff-scheduler/src/main/java/com/javanauta/bffscheduler/infrastructure/openapi/UserContactControllerOpenAPI.java
@@ -2,10 +2,13 @@ package com.javanauta.bffscheduler.infrastructure.openapi;
 
 import com.javanauta.bffscheduler.business.dto.request.ContactRequestDTO;
 import com.javanauta.bffscheduler.business.dto.response.ContactResponseDTO;
+import com.javanauta.bffscheduler.core.config.OpenAPIConfig;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Tag(name = "User Contact API", description = "API for managing contacts of the logged-in user")
+@SecurityRequirement(name = OpenAPIConfig.SECURITY_SCHEME)
 public interface UserContactControllerOpenAPI {
 
     @Operation(summary = "Find contacts of the logged-in user", description = "Retrieve the list of contacts for the logged-in user using a JWT token",
@@ -24,7 +28,7 @@ public interface UserContactControllerOpenAPI {
                             content = @Content)
             })
     @GetMapping
-    ResponseEntity<List<ContactResponseDTO>> findContacts(@RequestHeader("Authorization") String token);
+    ResponseEntity<List<ContactResponseDTO>> findContacts(@RequestHeader("Authorization") @Parameter(hidden = true) String token);
 
     @Operation(summary = "Create a new contact", description = "Create a new contact for the logged-in user",
             responses = {
@@ -38,7 +42,7 @@ public interface UserContactControllerOpenAPI {
             })
     @PostMapping
     ResponseEntity<ContactResponseDTO> createContact(@RequestBody ContactRequestDTO request,
-                                                     @RequestHeader("Authorization") String token);
+                                                     @RequestHeader("Authorization") @Parameter(hidden = true) String token);
 
     @Operation(summary = "Update an existing contact", description = "Update an existing contact for the logged-in user",
             responses = {
@@ -55,7 +59,7 @@ public interface UserContactControllerOpenAPI {
     @PutMapping("/{contactId}")
     ResponseEntity<ContactResponseDTO> updateContact(@RequestBody ContactRequestDTO request,
                                                      @PathVariable Long contactId,
-                                                     @RequestHeader("Authorization") String token);
+                                                     @RequestHeader("Authorization") @Parameter(hidden = true) String token);
 
     @Operation(summary = "Delete a contact", description = "Delete a contact of the logged-in user by contact ID",
             responses = {
@@ -67,5 +71,5 @@ public interface UserContactControllerOpenAPI {
             })
     @DeleteMapping("/{contactId}")
     ResponseEntity<Void> deleteContact(@PathVariable Long contactId,
-                                       @RequestHeader("Authorization") String token);
+                                       @RequestHeader("Authorization") @Parameter(hidden = true) String token);
 }
